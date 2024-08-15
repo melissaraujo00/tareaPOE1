1cc0031389cc3ce5432e6fc9a105b51b2178865c
melissa <daliarivas11@gmail.com>
2024-08-14 18:55:12 -0600

    Ejercicio 1, saber si la tecla digitada es un numero

---
+++ TeclaNumeroApp/TeclaNumeroUI/TeclaNumForms.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace TeclaNumeroUI
+{
+    partial class TeclaNumForms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tituloLabel = new Label();
+            teclaNumTextBox = new TextBox();
+            respuestaLabel = new Label();
+            SuspendLayout();
+            // 
+            // tituloLabel
+            // 
+            tituloLabel.AutoSize = true;
+            tituloLabel.Font = new Font("Segoe UI", 28F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            tituloLabel.Location = new Point(201, 9);
+            tituloLabel.Name = "tituloLabel";
+            tituloLabel.Size = new Size(393, 74);
+            tituloLabel.TabIndex = 0;
+            tituloLabel.Text = "Tecla Número";
+            // 
+            // teclaNumTextBox
+            // 
+            teclaNumTextBox.Location = new Point(63, 108);
+            teclaNumTextBox.Multiline = true;
+            teclaNumTextBox.Name = "teclaNumTextBox";
+            teclaNumTextBox.Size = new Size(675, 231);
+            teclaNumTextBox.TabIndex = 1;
+            teclaNumTextBox.KeyPress += teclaNumTextBox_KeyPress;
+            // 
+            // respuestaLabel
+            // 
+            respuestaLabel.AutoSize = true;
+            respuestaLabel.Location = new Point(308, 368);
+            respuestaLabel.Name = "respuestaLabel";
+            respuestaLabel.Size = new Size(0, 25);
+            respuestaLabel.TabIndex = 2;
+            // 
+            // TeclaNumForms
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(respuestaLabel);
+            Controls.Add(teclaNumTextBox);
+            Controls.Add(tituloLabel);
+            Name = "TeclaNumForms";
+            Text = "TeclaNumForms";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label tituloLabel;
+        private TextBox teclaNumTextBox;
+        private Label respuestaLabel;
+    }
+}
---
+++ TeclaNumeroApp/TeclaNumeroUI/TeclaNumForms.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TeclaNumeroUI
+{
+    public partial class TeclaNumForms : Form
+    {
+        public TeclaNumForms()
+        {
+            InitializeComponent();
+        }
+
+        private void teclaNumTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            try
+            {
+                if (char.IsDigit(e.KeyChar))
+                {
+                    respuestaLabel.Text = $"Se presionó el número: {e.KeyChar}";
+                }
+                else
+                {
+                    respuestaLabel.Text = "";
+                }
+            }
+            catch (Exception ex)
+            {
+                respuestaLabel.Text = "Se ha producido un error: " + ex.Message;
+            }
+        }
+    }
+}
